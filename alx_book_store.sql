fe29cc5310fe65608b569974804c5776a28893ab
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-06-24 20:08:04 +0300

    create database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,29 +1,29 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store
 USE  alx_book_store;
-CREATE TABLE IF NOT EXISTS authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id INTEGER PRIMARY KEY,
 author_name VARCHAR(215)
 );
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INTEGER PRIMARY KEY,
     title VARCHAR(130),
     author_id INTEGER,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id));
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 
-    CREATE TABLE customers(
+    CREATE TABLE Customers(
         customer_id INTEGER PRIMARY KEY,
         customer_name VARCHAR (215),
         email VARCHAR(215),
         address TEXT
     );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INTEGER PRIMARY KEY,
    customer_id INTEGER,
    order_date DATE,
-   FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+   FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
@@ -31,6 +31,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INTEGER,
     book_id INTEGER,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
