9db4faf2bfec943c1221b8c67300b974955a33e8
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-06-25 11:11:31 +0300

    run all tables

--- task_3.sql
+++ task_3.sql
@@ -1,11 +1,2 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="gat@123",
-    database = "alx_book_store"
-   
-)
-
+SHOW TABLES;
 mysql -u root -p alx_book_store -e "source task_3.sql"
